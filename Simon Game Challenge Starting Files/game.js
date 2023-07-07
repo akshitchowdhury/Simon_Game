8d2be7923f5fc87309fef99a56011881e0270e7f
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-07 12:29:32 +0530

    Add files via upload

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,9 +1,22 @@
 // alert("WC to the Simon game")
 
+var gamePattern = []
+
+
+
+var buttoncolors = ["red", "blue", "green", "yellow"]
+
+
+
 function nextSequence(){
 
 var randomnumber = Math.floor(Math.random()*4)
 
+
+
+
+var randomChosenColour = buttoncolors[randomnumber]
+
+gamePattern.push(randomChosenColour)
 }
 
-nextSequence()
