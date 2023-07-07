9b40e112dbec59b65ff1728531f7ee261b392242
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-07 14:27:35 +0530

    Add files via upload

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,6 @@
 // alert("WC to the Simon game")
+var userClickedPattern = [];
+
 
 var gamePattern = []
 
@@ -6,6 +8,21 @@ var gamePattern = []
 
 var buttoncolors = ["red", "blue", "green", "yellow"]
 
+
+
+$(".btn").click(function (){
+    
+    
+    var userChosenColor =   $(this).attr("id");
+
+    userClickedPattern.push(userChosenColor)
+    
+
+    playSound(userChosenColor)
+
+animatePress(userChosenColor)
+
+})
 
 
 function nextSequence(){
@@ -18,10 +35,35 @@ var randomnumber = Math.floor(Math.random()*4)
 var randomChosenColour = buttoncolors[randomnumber]
 
 gamePattern.push(randomChosenColour)
+
+
+$("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+
+
+// let audio = new Audio("./sounds/"+randomChosenColour+".mp3")
+
+// audio.play()
+
+playSound(randomChosenColour)
+
+
 }
 
-let audio = new Audio("./sounds/"+randomChosenColour+".mp3")
+function playSound(name){
+   
+    let audio = new Audio("sounds/"+ name+ ".mp3")
 
+    audio.play()
+    
+    
+}
 
+function animatePress(currentColour){
 
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100).play()
+    $("#"+currentColour).addClass("pressed")
+
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed")
+    },100)
+}
+
